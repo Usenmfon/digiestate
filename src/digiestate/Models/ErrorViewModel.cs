b2f32420736b3cd8cd658dcd8b2b66bf33d0e7cf
Usenmfon <usenmfonuko@gmail.com>
2020-06-08 06:58:47 +0100

    Added project

---
+++ src/digiestate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace digiestate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
