deea79f415dca4e74c73c1f708f1b93e1fae1bc4
Usenmfon <usenmfonuko@gmail.com>
2020-06-13 12:19:11 +0100

    created user roles

--- src/digiestate.Web/Startup.cs
+++ src/digiestate.Web/Startup.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Threading.Tasks;
 using digiestate.Data.DatabaseContexts.ApplicationContext;
 using digiestate.Data.DatabaseContexts.AuthenticationDbContext;
+using digiestate.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,11 +39,24 @@ namespace digiestate
                     sqlServerOptions.MigrationsAssembly("digiestate.Data");
                 }));
 
+                services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AuthenticationDbContext>()
+                .AddDefaultTokenProviders();
+
+                services.Configure<IdentityOptions>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.SignIn.RequireConfirmedEmail = false;
+                    options.SignIn.RequireConfirmedPhoneNumber = false;
+                });
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp)
         {
             if (env.IsDevelopment())
             {
@@ -64,6 +81,51 @@ namespace digiestate
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            MigrateDatabaseContexts(svp);
+            CreateDefaultRolesAndUsers(svp).GetAwaiter().GetResult();
+        }
+
+        public void MigrateDatabaseContexts(IServiceProvider svp)
+        {
+                var authenticationDbContext = svp.GetRequiredService<AuthenticationDbContext>();
+                authenticationDbContext.Database.Migrate();
+
+                var ApplicationDbContext = svp.GetRequiredService<ApplicationDbContext>();
+                ApplicationDbContext.Database.Migrate();
+        }
+
+        public async Task CreateDefaultRolesAndUsers(IServiceProvider svp)
+        {
+                string[] roles =  new string[] { "SystemAdministrator", "Agent", "User"};
+                var userEmail = "admin@digiestate.com";
+                var userPassword = "SuperSecretPassword@2020";
+
+                var roleManager = svp.GetRequiredService<RoleManager<IdentityRole>>();
+                foreach(var role in roles)
+                {
+                    var roleExists = await roleManager.RoleExistsAsync(role);
+                    if(!roleExists)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole{ Name = role});
+                    }
+                }
+
+                var userManager = svp.GetRequiredService<UserManager<ApplicationUser>>();
+                var user = await userManager.FindByEmailAsync(userEmail);
+                if(user is null)
+                {
+                    user = new ApplicationUser
+                    {
+                        Email = userEmail,
+                        UserName = userEmail,
+                        EmailConfirmed = true,
+                        PhoneNumber = "+2347063735920",
+                        PhoneNumberConfirmed = true
+                    };
+                    await userManager.CreateAsync(user, userPassword);
+                    await userManager.AddToRolesAsync(user, roles);
+                }
+                
         }
     }
 }
